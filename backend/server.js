7ba479c779b4f682ae5bad455ee1575ca52de206
Ifrah Abid <ifrahabidm24@gmail.com>
2026-09-09 18:03:08 -0800

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -27,8 +26,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
   })
   .catch((error) => {
